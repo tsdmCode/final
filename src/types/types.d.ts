91a6d2da4312a73be1d593ee4f17f44fd00a244a
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2026-09-15 11:49:06 +0200

    Fixes #17 hackede også backend så den kan lave jobcount nu, types opdateret accordingly

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -69,86 +69,90 @@ export interface Category {
 }
 
 export interface CategoryByID {
-    id:          number;
-    name:        string;
-    jobListings: JobListing[];
+  id: number;
+  name: string;
+  jobListings: JobListing[];
 }
 
 export interface JobListing {
-    id:            number;
-    createdAt:     Date;
-    description:   string;
-    title:         string;
-    address:       string;
-    zipcode:       number;
-    organization:  string;
-    city:          string;
-    workHome:      WorkHome;
-    regionId:      number;
-    userId:        number;
-    jobCategoryId: number;
-    workTypeId:    number;
-    region:        JobCategory;
-    workType:      WorkType;
-    jobCategory:   JobCategory;
+  id: number;
+  createdAt: Date;
+  description: string;
+  title: string;
+  address: string;
+  zipcode: number;
+  organization: string;
+  city: string;
+  workHome: WorkHome;
+  regionId: number;
+  userId: number;
+  jobCategoryId: number;
+  workTypeId: number;
+  region: JobCategory;
+  workType: WorkType;
+  jobCategory: JobCategory;
 }
 
 export interface JobCategory {
-    id:   number;
-    name: string;
+  id: number;
+  name: string;
+}
+
+export interface JobCategoryWithCount extends JobCategory {
+  jobCount: number;
 }
 
 export enum WorkHome {
-    Hybrid = "Hybrid",
-    OnSite = "On-site",
-    Remote = "Remote",
+  Hybrid = 'Hybrid',
+  OnSite = 'On-site',
+  Remote = 'Remote',
 }
 
 export interface WorkType {
-    id:   number;
-    type: Type;
+  id: number;
+  type: Type;
 }
 
 export enum Type {
-    Deltid = "Deltid",
-    Flex = "Flex",
+  Deltid = 'Deltid',
+  Flex = 'Flex',
 }
 
 export interface Article {
-    id:        number;
-    createdAt: Date;
-    title:     string;
-    content:   string;
-    author:    string;
-    imageUrl:  string;
+  id: number;
+  createdAt: Date;
+  title: string;
+  content: string;
+  author: string;
+  imageUrl: string;
 }
 
 export interface NewsletterSub {
-    id:    number;
-    email: string;
+  id: number;
+  email: string;
 }
 
 export interface Testimony {
-    id:      number;
-    name:    string;
-    title:   string;
-    content: string;
+  id: number;
+  name: string;
+  title: string;
+  content: string;
 }
 
 export interface Region {
-    id:   number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export interface User {
-    id:           number;
-    firstname:    string;
-    lastname:     string;
-    password:     string;
-    email:        string;
-    phone:        number;
-    address:      string;
-    city:         string;
-    zipcode:      number;
-    refreshToken: string;
+  id: number;
+  firstname: string;
+  lastname: string;
+  password: string;
+  email: string;
+  phone: number;
+  address: string;
+  city: string;
+  zipcode: number;
+  refreshToken: string;
 }
